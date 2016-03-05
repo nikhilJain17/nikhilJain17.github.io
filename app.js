885aa75f3d2bcdc34646b3b5463ce4e0fdecaf72
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-03-04 22:18:16 -0500

    added particles.js

--- app.js
+++ app.js
@@ -1,51 +1,133 @@
-particlesJS.load('particles-js', './particles.json', function() {
-  console.log('callback - particles.js config loaded');
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
 });
+*/
 
+/* Otherwise just put the config content (json): */
 
-//particlesJS('particlesjs', {
-//  particles: {
-//    color: '#e6e6e6',
-//    shape: 'circle', // "circle", "edge" or "triangle"
-//    opacity: 1,
-//    size: 4,
-//    size_random: true,
-//    nb: 150,
-//    line_linked: {
-//      enable_auto: true,
-//      distance: 100,
-//      color: '#e6e6e6',
-//      opacity: 1,
-//      width: 1,
-//      condensed_mode: {
-//        enable: false,
-//        rotateX: 600,
-//        rotateY: 600
-//      }
-//    },
-//    anim: {
-//      enable: true,
-//      speed: 1
-//    }
-//  },
-//  interactivity: {
-//    enable: true,
-//    mouse: {
-//      distance: 300
-//    },
-//    detect_on: 'window', // "canvas" or "window"
-//    mode: 'grab',
-//    line_linked: {
-//      opacity: .5
-//    },
-//    events: {
-//      onclick: {
-//        enable: true,
-//        mode: 'push', // "push" or "remove"
-//        nb: 4
-//      }
-//    }
-//  },
-//  /* Retina Display Support */
-//  retina_detect: true
-//});
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "impulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push" // help
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
